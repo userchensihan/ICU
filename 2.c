d01e914f7a09d3a88a7e7242596b6f8a7be21bed
userchensihan <40167434@qq.com>
2025-07-19 12:23:24 +0800

    Add files via upload

---
+++ 2.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+int main() {
+    int s;
+    struct sockaddr_in e;
+    char r[1024];
+    char p[128];
+    s=socket(AF_INET,SOCK_STREAM,0);    
+    memset(&e, 0, sizeof(e));
+    e.sin_family = AF_INET;
+    e.sin_port = htons(80);
+    inet_pton(AF_INET,"192.168.1.1",&e.sin_addr);
+    if (connect(s,(struct sockaddr *)&e, sizeof(e)) < 0) {
+        perror("Connection failed");
+        close(s);
+        exit(EXIT_FAILURE);
+    }
+    snprintf(r, sizeof(r), 
+             "POST /cgi-bin/luci HTTP/1.1\r\n"
+	     "Content-Type: application/x-www-form-urlencoded\r\n"
+	     "Content-Length: 23\r\n"
+	     "Host: 192.168.1.1\r\n"
+             "Connection: close\r\n\r\n"             
+             "username=useradmin&psd=" 
+             );
+    send(s,r,strlen(r),0); 
+    //ssize_t u;
+    //while ((u = recv(s, p, 127, 0)) > 0) {
+    //    p[u] = '\0'; 
+    //   printf("%s", p);
+    //} 
+    close(s);
+    return 0;
+}
