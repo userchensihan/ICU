544993d4005b0662887c68a0dad2debd8377996f
userchensihan <40167434@qq.com>
2025-07-19 10:07:14 +0800

    Update 0.py

--- 0.py
+++ 0.py
@@ -2,6 +2,6 @@ from scapy.all import *
 c=conf.route.route("0.0.0.0")[2]
 p=Ether(dst="ff:ff:ff:ff:ff:ff",src="EC:91:61:89:9F:EF")/ARP(pdst=c)
 a=srp(p,verbose=0)
-for i in a[0].res:
-    b=i.answer
+for u in a[0].res:
+    b=u.answer
     print(b.src)
