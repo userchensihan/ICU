4ad3a323ebeb5cfca440cd7d81fb83239605dd79
userchensihan <40167434@qq.com>
2025-07-20 20:51:35 +0800

    Add files via upload

---
+++ WallPaper.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <windows.h>
+#include <cstdio>
+using namespace std;
+int main ()
+{
+	char p[MAX_PATH];
+	for(int a=1;a<520;a++)
+	{
+		snprintf(p,MAX_PATH,"C:\\Users\\Administrator\\Desktop\\%d.bmp",a);
+		Sleep(520);	
+		SystemParametersInfoA(SPI_SETDESKWALLPAPER,0,p,SPIF_UPDATEINIFILE);
+	}
+	return 0;
+}
