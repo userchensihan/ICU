f3c8f9417cf26a450195b98c392cbd652caf8bd3
userchensihan <40167434@qq.com>
2025-07-20 10:15:42 +0800

    Add files via upload

---
+++ 2.cpp
@@ -0,0 +1,22 @@
+#include <windows.h>
+#include <iostream>
+#include <psapi.h>
+using namespace std;
+int main()
+{
+    DWORD b[512];
+    DWORD a;
+    DWORD cProcesses;
+    unsigned int l;
+    EnumProcesses(b, sizeof(b), &a);
+    cProcesses = a / sizeof(DWORD);
+    for ( l = 0; l < cProcesses; l++ )
+    {
+        if( b[l] != 0 )
+        {
+            int c = b[l];
+            cout << c << endl;
+        }
+    }
+    return 0;
+}
